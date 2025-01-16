4ab8407a22b314fa6db65742fc4ed76f1c20c759
Samuel Huwiler <Samuel_huwiler@sluz.ch>
2025-01-16 12:42:45 +0100

    Added Logging

--- src/main/java/ch/ksrminecraft/testAPI/TestAPI.java
+++ src/main/java/ch/ksrminecraft/testAPI/TestAPI.java
@@ -6,6 +6,7 @@ public final class TestAPI extends JavaPlugin {
     TestManager testManager;
     @Override
     public void onEnable() {
+        System.out.println("Starting TestAPI");
         testManager = new TestManager();
         // Plugin startup logic
 
